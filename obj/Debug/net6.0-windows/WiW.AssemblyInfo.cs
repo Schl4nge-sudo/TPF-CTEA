5e5df3db7f0a9b8780ccf1bb39f5d5a7ede52fb4
Schl4nge-sudo <angelschafer03@gmail.com>
2025-06-11 00:27:22 -0300

    Añadiendo capturas para el Readme

--- obj/Debug/net6.0-windows/WiW.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/WiW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WiW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c697ef127b4a995280520feeecc9ce7af171fec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8db0f7721eb2fdee63816df6bbcf0ca6a2dd36a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WiW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WiW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
